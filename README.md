7007cf6f4dea4fdaee3ca6fddb4dbb8a7cf2e83c
Chris Metcalf <chris@chrismetcalf.net>
2016-06-07 13:42:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A quick example of how to make use of the [FCC Consumer Complaints Database](https://dev.socrata.com/foundry/opendata.fcc.gov/sr6c-syda) open API to look up whether or not there have been complaints about a particular number.
+
+Live example at <http://chrismetcalf.github.io/stop-calling-me/>!
---
+++ js/stop-calling-me.js
@@ -0,0 +1,31 @@
+$(function() {
+  $("#search").submit(function(event) {
+    event.preventDefault();
+    var number = $("#search .query").val();
+
+    // Clean up our number
+    number = number
+      .replace(/[^0-9]/g, '')
+      .replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+
+    // Grab our current location and fetch nearby restaurants
+    $.ajax({
+      url: "https://opendata.fcc.gov/resource/sr6c-syda.json",
+      type: "GET",
+      data: {
+        "issue" : "Telemarketing (including do not call and spoofing)",
+        "caller_id_number" : number,
+        "$select" : "caller_id_number, COUNT(*) AS count, MAX(issue_date) AS last_reported",
+        "$group" : "caller_id_number",
+        "$$app_token": "VNyBRMoJAPyaGMUC42IY2M1UA"
+      }
+    }).done(function(calls) {
+      if(calls.length > 0) {
+        $("#results").text("There have been " + calls[0].count + " complaint(s) about this number, most recently at " + (new Date(calls[0].last_reported)).toLocaleString());
+      } else {
+        $("#results").text("No complaints yet for this number...");
+      }
+    });
+  });
+});
+
